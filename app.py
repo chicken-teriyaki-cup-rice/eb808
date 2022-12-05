126c2d45597d297c85522dabaa2860a9e162cc26
walrusautotune <walrusautotune@protonmail.com>
2022-12-05 04:00:00 +0100

    adds home and about method

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ from api import API
 
 
 app = API()
+
+
+def home(request, response):
+    response.text = "Hello from the home page"
+
+
+def about(request, response):
+    response.text = "Hello from the about page"

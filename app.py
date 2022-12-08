fe0f045018f19f1a5f221ebda7cf3bfde5d08549
walrusautotune <walrusautotune@protonmail.com>
2022-12-08 03:00:00 +0100

    adds decorators

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from api import API
 app = API()
 
 
+@app.route("/home")
 def home(request, response):
     response.text = "Hello from the home page"
 
 
+@app.route("/about")
 def about(request, response):
     response.text = "Hello from the about page"

baf3c0d864457b8b7d5fa6a9a6bb19bec44fb70d
walrusautotune <walrusautotune@protonmail.com>
2022-12-12 03:30:00 +0100

    fixes find_handler method

--- api.py
+++ api.py
@@ -13,10 +13,6 @@ class API:
 
         return response(environ, start_response)
 
-    def default_response(self, response):
-        response.status_code = 404
-        response.text = "Not found."
-
     def route(self, path):
         def wrapper(handler):
             self.routes[path] = handler
@@ -24,11 +20,17 @@ class API:
 
         return wrapper
 
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+
     def find_handler(self, request_path):
         for path, handler in self.routes.items():
             parse_result = parse(path, request_path)
             if parse_result is not None:
                 return handler, parse_result.named
+
+        return None, None
 
     def handle_request(self, request):
         response = Response()

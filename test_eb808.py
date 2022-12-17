a5740c5fe597df25272f48af189a4ef40eb0f8f1
walrusautotune <walrusautotune@protonmail.com>
2022-12-17 03:30:00 +0100

    adds testing routes

--- test_eb808.py
+++ test_eb808.py
@@ -6,3 +6,9 @@ from api import API
 @pytest.fixture
 def api():
     return API()
+
+
+def test_basic_route_adding(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "Hello World!"

64244f858b5c302c3899a0058ce61dd630c9a9fd
walrusautotune <walrusautotune@protonmail.com>
2022-12-17 04:30:00 +0100

    adds session test

--- test_eb808.py
+++ test_eb808.py
@@ -19,3 +19,13 @@ def test_route_adding_with_same_path(api):
         @api.route("/home")
         def home2(req, resp):
             resp.text = "Hello World!"
+
+
+def test_framework_test_client_can_send_requests(api, test_client):
+    RESPONSE_TEXT = "Hello World!"
+
+    @api.route("/hey")
+    def hey(req, resp):
+        resp.text = RESPONSE_TEXT
+
+    assert test_client.get("http://testserver/hey").text == RESPONSE_TEXT

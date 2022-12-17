e201403a12e508b50c4a8640dfe04a75444b231b
walrusautotune <walrusautotune@protonmail.com>
2022-12-17 03:50:00 +0100

    adds duplicate routes test

--- test_eb808.py
+++ test_eb808.py
@@ -12,3 +12,15 @@ def test_basic_route_adding(api):
     @api.route("/home")
     def home(req, resp):
         resp.text = "Hello World!"
+
+
+def test_route_adding_with_same_path(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "Hello World!"
+
+    with pytest.raises(AssertionError):
+
+        @api.route("/home")
+        def home2(req, resp):
+            resp.text = "Hello World!"

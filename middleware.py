c4a6fa08d15fd743c8693a60081fdb4dab774581
walrusautotune <walrusautotune@protonmail.com>
2023-01-02 03:00:00 +0100

    adds req/resp processing methods

--- middleware.py
+++ middleware.py
@@ -2,6 +2,11 @@ class Middleware:
     def __init__(self, app):
         self.app = app
 
+    def add(self, middleware_cls):
+        self.app = middleware_cls(self.app)
 
-def add(self, middleware_cls):
-    self.app = middleware_cls(self.app)
+    def process_request(self, req):
+        pass
+
+    def process_response(self, req, resp):
+        pass

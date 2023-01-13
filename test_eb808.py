0e0b6bf51d31b26618d127bf1da08e3435f84b49
walrusautotune <walrusautotune@protonmail.com>
2023-01-13 04:10:00 +0100

    adds test_manually_setting_body

--- test_eb808.py
+++ test_eb808.py
@@ -229,3 +229,15 @@ def test_text_response_helper(api, client):
 
     assert "text/plain" in response.headers["Content-Type"]
     assert response.text == response_text
+
+
+def test_manually_setting_body(api, client):
+    @api.route("/body")
+    def text_handler(req, resp):
+        resp.body = b"Byte Body"
+        resp.content_type = "text/plain"
+
+    response = client.get("http://testserver/body")
+
+    assert "text/plain" in response.headers["Content-Type"]
+    assert response.text == "Byte Body"

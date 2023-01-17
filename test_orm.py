9657ac9a775967a2da073f292c7c5502573ee23a
walrusautotune <walrusautotune@protonmail.com>
2023-01-17 03:00:00 +0100

    adds test

--- test_orm.py
+++ test_orm.py
@@ -1,3 +1,10 @@
 import sqlite3
 
-from eb808.orm import Database as db
+from eb808.orm import Database
+
+
+def test_create_db():
+    db = Database("./test.db")
+
+    assert isinstance(db.conn, sqlite3.Connection)
+    assert db.tables == []
